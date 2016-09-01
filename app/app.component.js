feb05a3063f4d2029fe9c3445bf3e392f386ac1c
Lucky <175917645@qq.com>
2016-09-01 23:24:17 +0800

    添加router

--- app/app.component.js
+++ app/app.component.js
@@ -9,33 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var hero_service_1 = require('./hero.service');
 var AppComponent = (function () {
-    function AppComponent(heroService) {
-        this.heroService = heroService;
+    function AppComponent() {
         this.title = 'Tour of Heroes';
     }
-    ;
-    AppComponent.prototype.getHeroes = function () {
-        var _this = this;
-        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
-    };
-    AppComponent.prototype.ngOnInit = function () {
-        this.getHeroes();
-    };
-    AppComponent.prototype.onSelect = function (hero) {
-        this.selectHero = hero;
-    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n\t\t<h2>My Heroes</h2>\n\t\t<ul class=\"heroes\" >\n\t\t\t<li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\" [class.selected]=\"hero === selectHero\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n\t\t</ul>\n\t\t<my-hero-detail [hero]=\"selectHero\"></my-hero-detail>",
-            styles: [
-                "\n.selected {\nbackground-color: #CFD8DC !important;\ncolor: white;\n}\n.heroes {\nmargin: 0 0 2em 0;\nlist-style-type: none;\npadding: 0;\nwidth: 15em;\n}\n.heroes li {\ncursor: pointer;\nposition: relative;\nleft: 0;\nbackground-color: #EEE;\nmargin: .5em;\npadding: .3em 0;\nheight: 1.6em;\nborder-radius: 4px;\n}\n.heroes li.selected:hover {\nbackground-color: #BBD8DC !important;\ncolor: white;\n}\n.heroes li:hover {\ncolor: #607D8B;\nbackground-color: #DDD;\nleft: .1em;\n}\n.heroes .text {\nposition: relative;\ntop: -3px;\n}\n.heroes .badge {\ndisplay: inline-block;\nfont-size: small;\ncolor: white;\npadding: 0.8em 0.7em 0 0.7em;\nbackground-color: #607D8B;\nline-height: 1em;\nposition: relative;\nleft: -1px;\ntop: -4px;\nheight: 1.8em;\nmargin-right: .8em;\nborder-radius: 4px 0 0 4px;\n}\n"
-            ],
-            providers: [hero_service_1.HeroService]
+            template: "\n\t\t<h1>{{title}}</h1>\n\t\t<nav>\n\t\t<a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n\t\t<a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n\t\t</nav>\n\t\t<router-outlet></router-outlet>\n\t",
+            styleUrls: ['app/app.component.css']
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
